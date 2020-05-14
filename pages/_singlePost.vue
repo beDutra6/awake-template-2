557a413575d1020a8b657e73ad16ce3b1f04ee72
bruno dutra <bruno.d@kzsoftworks.com>
2020-05-14 00:26:05 -0300

    form on post

--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -23,6 +23,17 @@
             <posts-grid :number="3" :category="category" :exclude="slug" />
           </div>
           <disqus-comments :identifier="$route.params.singlePost" />
+          <form name="contact" netlify>
+            <p>
+              <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
       </template>
       <template v-slot:sidebar>
